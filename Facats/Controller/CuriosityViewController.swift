092ba25a240f5444d4fa9ccfcdda3f4a0c40616d
Larissa Uchôa <ltuchoa@gmail.com>
2020-08-13 09:40:25 -0300

    added background

--- Facats/Controller/CuriosityViewController.swift
+++ Facats/Controller/CuriosityViewController.swift
@@ -12,19 +12,16 @@ class CuriosityViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        setupBackground()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupBackground() {
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        
     }
-    */
 
 }
