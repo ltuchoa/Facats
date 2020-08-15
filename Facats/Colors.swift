35ea93686a2b4aeb62270cf7e3a162e6fd1a57f7
Larissa Uchôa <ltuchoa@gmail.com>
2020-08-15 14:39:27 -0300

    interface almost done

--- Facats/Colors.swift
+++ Facats/Colors.swift
@@ -11,4 +11,5 @@ import UIKit
 extension UIColor {
     static let actionColor = UIColor(red: 0.94, green: 0.55, blue: 0.32, alpha: 1.00)
     static let unselectedColor = UIColor(red: 64/255, green: 63/255, blue: 63/255, alpha: 1.00)
+    static let segmentedColor = UIColor(red: 0.94, green: 0.55, blue: 0.32, alpha: 0.35)
 }
--- Facats/Controller/BreedsViewController.swift
+++ Facats/Controller/BreedsViewController.swift
@@ -10,23 +10,57 @@ import UIKit
 
 class BreedsViewController: UIViewController {
 
+    var segmentedControl = UISegmentedControl()
+    var searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Breeds"
+        
+        setupSegmentedControl()
+        setupSearchBar()
+        
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
-    */
-
+    
+    func setupSegmentedControl() {
+        let items = ["All", "Favorites"]
+        segmentedControl = UISegmentedControl(items: items)
+        
+        segmentedControl.selectedSegmentTintColor = .actionColor
+        segmentedControl.backgroundColor = .segmentedColor
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 8),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+        
+        //segmentedControl.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+    }
+    
+    func setupSearchBar() {
+        searchBar.barTintColor = .white
+        searchBar.backgroundImage = UIImage()
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar)
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+    
 }
